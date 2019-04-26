7b55795a390d703cbbc4eeb8cc5e5dda5cf9b2c8
Josh Kramer <josh.kramer@measuresforjustice.org>
2019-04-25 23:17:32 -0400

    DEMO: Create a fake state implementation to use in a functional component DO NOT DO THIS IN A REAL APP

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import useForceUpdate from './useForceUpdate'
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,17 +12,96 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <HeroLink text='Learn React' />
       </header>
     </div>
   );
 }
+
+
+// DO NOT DO THIS!!!
+// THIS IS A DEMONSTRATION OF WHAT REACT IS *SORT OF*
+// DOING IN THE BACKGROUND FOR useState() HOOK. DO NOT
+// REPLECATE THIS METHOD.
+let counter = 0
+let setCounter = (newVal) => {
+  counter = newVal
+  // Somehow tell React to re-render
+}
+// end of DO NOT USE THIS
+
+
+const HeroLink = (props) => {
+
+  const { text } = props
+  const forceUpdate = useForceUpdate()
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCounter(counter + 1)
+          forceUpdate()
+        }}
+      >Increment</button> - {counter}
+      <br />
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {text}
+      </a>
+    </div>
+  )
+}
+
+// // Class example of component state and lifecycle methods
+// class HeroLink extends React.Component {
+
+//   state = { red: true, counter: 0 }
+
+//   componentDidMount() {
+//     this.timeout = setTimeout(() => {
+//       this.setState({ red: false })
+//     }, 2000)
+//   }
+
+//   componentDidUpdate() {
+//     this.timeout = setTimeout(() => {
+//       this.setState({ red: !this.state.red })
+//     }, 2000)
+//   }
+
+//   componentWillUnmount() {
+//     clearTimeout(this.timeout)
+//   }
+
+//   render() {
+//     const { text } = this.props
+
+//     return (
+//       <div>
+//         <button
+//           onClick={() => {
+//             this.setState({ counter: this.state.counter + 1})
+//           }}
+//         >Increment</button> - {this.state.counter}
+//         <br />
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//           style={{ background: this.state.red ? 'red' : 'transparent' }}
+//         >
+//           {text}
+//         </a>
+//       </div>
+//     )
+//   }
+// }
+
 
 export default App;
--- src/useForceUpdate.js
+++ src/useForceUpdate.js
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 
+// This is generally a bad idea. In most cases,
+// you should never need to trick React into
+// re-rendering
 const useForceUpdate = () => {
   const [, update] = useState(null)
 
