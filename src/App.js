678fe1b11830874cdf1758d13dffd00db3f1678d
Josh Kramer <josh.kramer@measuresforjustice.org>
2019-04-25 23:07:33 -0400

    DEMO: Demonstrate React.memo(): creating pure functional components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React, { useState, lazy, Suspense } from 'react'
-import Loading from './components/Loading'
+import React, { useState, memo } from 'react'
+// import Loading from './components/Loading'
 import './App.css';
 
-const Clock = lazy(() => import('./components/tabs/Clock'))
-const Timer = lazy(() => import('./components/tabs/Timer'))
+// const Clock = lazy(() => import('./components/tabs/Clock'))
+// const Timer = lazy(() => import('./components/tabs/Timer'))
 
 
 const App = () => {
+  console.log('Rendering App')
 
-  const [tab, setTab] = useState(null)
+  // const [tab, setTab] = useState(null)
+
+  const [first, setFirst] = useState(0)
+  const [second, setSecond] = useState(0)
 
   return (
     <>
-      <div className='App__nav'>
+      {/* <div className='App__nav'>
         {
           ['Clock', 'Timer'].map((item, index) => (
             <button
@@ -22,8 +26,12 @@ const App = () => {
             >{item}</button>
           ))
         }
-      </div>
-      <div className='App__body'>
+      </div> */}
+
+      <MyComponent id='1' val={first} setter={setFirst} />
+      <MyComponent id='2' val={second} setter={setSecond} />
+
+      {/* <div className='App__body'>
         <Suspense fallback={<Loading />}>
           {
             tab === 0 ? <Clock /> : null
@@ -32,14 +40,41 @@ const App = () => {
             tab === 1 ? <Timer /> : null
           }
         </Suspense>
-      </div>
+      </div> */}
     </>
   )
 }
 
+// // Pure class component using React.PureComponent
+// class MyComponent extends React.PureComponent {
+
+//   render() {
+//     console.log(`Rendering MyComponent, id=${this.props.id}`)
+
+//     return <div
+//       style={{ border: '1px solid black' }}
+//     >
+//       ID: {this.props.id},
+//       <button
+//         onClick={() => this.props.setter(this.props.val + 1)}
+//       >Increment</button> {this.props.val}
+//     </div>
+//   }
+// }
+
+// Pure functional component using React.memo
+const MyComponent = memo((props) => {
+  console.log(`Rendering MyComponent, id=${props.id}`)
+
+  return <div
+    style={{ border: '1px solid black' }}
+  >
+    ID: {props.id},
+    <button
+      onClick={() => props.setter(props.val + 1)}
+    >Increment</button> {props.val}
+  </div>
+})
+
 export default App
 
-// Use normal function so it gets hoisted to top
-function delay(fn) {
-  return () => (new Promise((resolve) => setTimeout(resolve, 1000))).then(fn)
-}
